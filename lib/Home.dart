1db7a363f709916c574aee82d68369c546463a91
Gustavo <gustavotch@hotmail.com>
2021-01-27 15:44:44 -0300

    Commit with DB saving and recovering notes functionalities

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:minhas_anotacoes/helper/AnotacaoHelper.dart';
+import 'package:minhas_anotacoes/model/Anotacao.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -9,11 +11,13 @@ class _HomeState extends State<Home> {
 
   TextEditingController _tituloController = TextEditingController();
   TextEditingController _descricaoController = TextEditingController();
+  var _db = AnotacaoHelper();
+  List<Anotacao> _anotacoes = List<Anotacao>();
 
   _exibirTelaCadastro(){
 
     showDialog(
-        context: null,
+        context: context,
       builder: (context) {
           return AlertDialog(
             title: Text("Adicionar Anotação"),
@@ -44,9 +48,8 @@ class _HomeState extends State<Home> {
               ),
               FlatButton(
                 onPressed: (){
-
                   //Salvar
-
+                  _salvarAnotacao();
                   Navigator.pop(context);
                 },
                 child: Text("Salvar"),
@@ -58,14 +61,76 @@ class _HomeState extends State<Home> {
     
   }
 
+  _salvarAnotacao() async {
+
+    String titulo = _tituloController.text;
+    String descricao = _descricaoController.text;
+
+    Anotacao anotacao = new Anotacao(titulo, descricao, DateTime.now().toString());
+    int resultado = await _db.salvarAnotacao(anotacao);
+    print("salvar: " + resultado.toString() );
+
+    _tituloController.clear();
+    _descricaoController.clear();
+
+    _recuperarAnotacoes();
+  }
+
+  _recuperarAnotacoes() async {
+
+    List anotacoesRecuperadas = await _db.recuperarAnotacoes();
+
+    List<Anotacao> listaTemporaria = List<Anotacao>();
+
+    for( var item in anotacoesRecuperadas ){
+
+      Anotacao anotacao = Anotacao.fromMap(item);
+      listaTemporaria.add( anotacao );
+    }
+
+    setState(() {
+      _anotacoes = listaTemporaria;
+    });
+    listaTemporaria = null;
+
+    print("lista " + anotacoesRecuperadas.toString());
+
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _recuperarAnotacoes();
+  }
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Minhas anotações"),
           backgroundColor: Colors.lightGreen,
       ),
-      body: Container(),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+              child: ListView.builder(
+                itemCount: _anotacoes.length,
+                  itemBuilder: (context, index){
+
+                  final anotacao = _anotacoes[index];
+
+                    return Card(
+                      child: ListTile(
+                        title: Text( anotacao.titulo ),
+                        subtitle: Text("${anotacao.data} - ${anotacao.descricao}" ),
+                      ),
+                    );
+                  }
+                  )
+              )
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.green,
         foregroundColor: Colors.white,
--- lib/helper/AnotacaoHelper.dart
+++ lib/helper/AnotacaoHelper.dart
@@ -1,8 +1,10 @@
+import 'package:minhas_anotacoes/model/Anotacao.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
 class AnotacaoHelper {
 
+  static final String nomeTabela = "anotacao";
   static final AnotacaoHelper _anotacaoHelper = AnotacaoHelper._internal();
   Database _db;
 
@@ -13,19 +15,40 @@ class AnotacaoHelper {
   AnotacaoHelper._internal () {
   }
 
+  Future<int> salvarAnotacao(Anotacao anotacao) async{
+
+    var bancoDados = await db;
+    int resultado = await bancoDados.insert( nomeTabela, anotacao.toMap() );
+    return resultado;
+  }
+
+  recuperarAnotacoes() async {
+
+    var bancoDados = await db;
+    String sql = "SELECT * FROM $nomeTabela ORDER BY data DESC";
+    List anotacoes = await bancoDados.rawQuery( sql );
+    return anotacoes;
+
+  }
+
   get db async {
 
     if( _db != null) {
       return _db;
     }
     else {
-
+      _db = await inicializarDB();
+      return _db;
     }
   }
 
   _onCreate(Database db, int version) async {
 
-    String sql = "CREATE TABLE aotacao (id INTEGER PRIMARY KEY AUTOINCREMENT, titulo VARCHAR, descricao TEXT, data DATETIME)";
+    String sql = "CREATE TABLE $nomeTabela ("
+        "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+        "titulo VARCHAR, "
+        "descricao TEXT, "
+        "data DATETIME)";
     await db.execute(sql);
 
   }
